455af1c7b54fe4dd5ee46ce240136b3d3f707bc2
BushraPrapti <bushraprapti151288@gmail.com>
2022-08-23 02:25:45 -0700

    added code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-const cartArray = [];
 
+
+const cartArray = [];
+function getVal() {
+  const val = document.querySelector('input').value;
+  return val;
+}
 function display(cartProduct) {
   console.log(cartProduct);
   const tableBody = document.getElementById("cart-playerName");
@@ -17,21 +22,43 @@ function display(cartProduct) {
 
   }
 }
+
+let count = 0;
 function select(element) {
 
-  console.log(element.parentNode.parentNode.children[0].innerText)
+  // console.log(element.parentNode.parentNode.children[0].innerText)
 
   const playerName = element.parentNode.parentNode.children[0].innerText;
-  // console.log(playerName)
+
+
   const playerObj = {
     playerName: playerName
   }
-  cartArray.push(playerObj);
+  if (count < 5) {
+    cartArray.push(playerObj);
+    const playerName1 = element.parentNode.parentNode.children[2].querySelectorAll("div.card-actions button")[0].disabled = true;
+  }
+  else {
+    alert("You can't select 5 more player")
+  }
 
-  // console.log(playerObj)
+
+
   // console.log(cartArray.length);
   document.getElementById("total-selected-player").innerText = cartArray.length;
+  if (cartArray.length > 0) {
+    document.getElementById("total-cost-player").innerText = cartArray.length * Number(getVal());
+  }
+
+
   display(cartArray);
+  count++;
 }
+if (cartArray.length === 0) {
+  document.getElementById("total-cost-player").innerText = getVal();
+}
+
+
+
 
 
